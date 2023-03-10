39f7bbf6c7382244627d4899ca9f42f9132b746b
TomeuGT <63001054+TomeuGT@users.noreply.github.com>
2023-03-10 13:26:33 +0100

    Agregar archivos de proyecto.

---
+++ Form1_Tomeu_Gomila/Form1.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace Form1_Tomeu_Gomila
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Hostname = new System.Windows.Forms.Label();
+            this.lbl_ip = new System.Windows.Forms.Label();
+            this.lbl_getaway = new System.Windows.Forms.Label();
+            this.lbl_ssid = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbl_ssid_status = new System.Windows.Forms.Label();
+            this.lbl_mac_address = new System.Windows.Forms.Label();
+            this.lbl_Internet_Connection = new System.Windows.Forms.Label();
+            this.lbl_VirtualBoxInstalled = new System.Windows.Forms.Label();
+            this.lbl_VBOX_V = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label12, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 69);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(3, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(770, 69);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "WELCOME TO VIRTUAL BOX MANAGEMENT";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(18, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP HOST:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(18, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "IP GETAWAY:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(18, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "USERNAME:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(21, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "HOSTNAME:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(21, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "SSID:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(377, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SSID STATUS:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(380, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "MAC ADDRESS:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(383, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "INTERNET CONNECTION:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(383, 187);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(144, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "IS VIRTUALBOX INSTALLED?";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Impact", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(386, 222);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(121, 17);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "VIRTUAL BOX VERSION:";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Username.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Username.Location = new System.Drawing.Point(90, 159);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(91, 16);
+            this.lbl_Username.TabIndex = 11;
+            this.lbl_Username.Text = "lbl_Username";
+            // 
+            // lbl_Hostname
+            // 
+            this.lbl_Hostname.AutoSize = true;
+            this.lbl_Hostname.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Hostname.Location = new System.Drawing.Point(93, 188);
+            this.lbl_Hostname.Name = "lbl_Hostname";
+            this.lbl_Hostname.Size = new System.Drawing.Size(90, 16);
+            this.lbl_Hostname.TabIndex = 12;
+            this.lbl_Hostname.Text = "lbl_Hostname";
+            // 
+            // lbl_ip
+            // 
+            this.lbl_ip.AutoSize = true;
+            this.lbl_ip.ForeColor = System.Drawing.Color.Red;
+            this.lbl_ip.Location = new System.Drawing.Point(76, 98);
+            this.lbl_ip.Name = "lbl_ip";
+            this.lbl_ip.Size = new System.Drawing.Size(39, 16);
+            this.lbl_ip.TabIndex = 13;
+            this.lbl_ip.Text = "lbl_ip";
+            // 
+            // lbl_getaway
+            // 
+            this.lbl_getaway.AutoSize = true;
+            this.lbl_getaway.ForeColor = System.Drawing.Color.Red;
+            this.lbl_getaway.Location = new System.Drawing.Point(96, 127);
+            this.lbl_getaway.Name = "lbl_getaway";
+            this.lbl_getaway.Size = new System.Drawing.Size(79, 16);
+            this.lbl_getaway.TabIndex = 14;
+            this.lbl_getaway.Text = "lbl_getaway";
+            // 
+            // lbl_ssid
+            // 
+            this.lbl_ssid.AutoSize = true;
+            this.lbl_ssid.ForeColor = System.Drawing.Color.Red;
+            this.lbl_ssid.Location = new System.Drawing.Point(64, 224);
+            this.lbl_ssid.Name = "lbl_ssid";
+            this.lbl_ssid.Size = new System.Drawing.Size(53, 16);
+            this.lbl_ssid.TabIndex = 15;
+            this.lbl_ssid.Text = "lbl_ssid";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(0, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(51, 16);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "label13";
+            // 
+            // lbl_ssid_status
+            // 
+            this.lbl_ssid_status.AutoSize = true;
+            this.lbl_ssid_status.ForeColor = System.Drawing.Color.Red;
+            this.lbl_ssid_status.Location = new System.Drawing.Point(459, 97);
+            this.lbl_ssid_status.Name = "lbl_ssid_status";
+            this.lbl_ssid_status.Size = new System.Drawing.Size(95, 16);
+            this.lbl_ssid_status.TabIndex = 17;
+            this.lbl_ssid_status.Text = "lbl_ssid_status";
+            // 
+            // lbl_mac_address
+            // 
+            this.lbl_mac_address.AutoSize = true;
+            this.lbl_mac_address.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_mac_address.ForeColor = System.Drawing.Color.Red;
+            this.lbl_mac_address.Location = new System.Drawing.Point(462, 126);
+            this.lbl_mac_address.Name = "lbl_mac_address";
+            this.lbl_mac_address.Size = new System.Drawing.Size(111, 16);
+            this.lbl_mac_address.TabIndex = 18;
+            this.lbl_mac_address.Text = "lbl_mac_address";
+            // 
+            // lbl_Internet_Connection
+            // 
+            this.lbl_Internet_Connection.AutoSize = true;
+            this.lbl_Internet_Connection.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Internet_Connection.Location = new System.Drawing.Point(513, 158);
+            this.lbl_Internet_Connection.Name = "lbl_Internet_Connection";
+            this.lbl_Internet_Connection.Size = new System.Drawing.Size(145, 16);
+            this.lbl_Internet_Connection.TabIndex = 19;
+            this.lbl_Internet_Connection.Text = "lbl_Internet_Connection";
+            // 
+            // lbl_VirtualBoxInstalled
+            // 
+            this.lbl_VirtualBoxInstalled.AutoSize = true;
+            this.lbl_VirtualBoxInstalled.ForeColor = System.Drawing.Color.Red;
+            this.lbl_VirtualBoxInstalled.Location = new System.Drawing.Point(533, 188);
+            this.lbl_VirtualBoxInstalled.Name = "lbl_VirtualBoxInstalled";
+            this.lbl_VirtualBoxInstalled.Size = new System.Drawing.Size(138, 16);
+            this.lbl_VirtualBoxInstalled.TabIndex = 20;
+            this.lbl_VirtualBoxInstalled.Text = "lbl_VirtualBoxInstalled";
+            // 
+            // lbl_VBOX_V
+            // 
+            this.lbl_VBOX_V.AutoSize = true;
+            this.lbl_VBOX_V.ForeColor = System.Drawing.Color.Red;
+            this.lbl_VBOX_V.Location = new System.Drawing.Point(516, 222);
+            this.lbl_VBOX_V.Name = "lbl_VBOX_V";
+            this.lbl_VBOX_V.Size = new System.Drawing.Size(80, 16);
+            this.lbl_VBOX_V.TabIndex = 21;
+            this.lbl_VBOX_V.Text = "lbl_VBOX_V";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_VBOX_V);
+            this.Controls.Add(this.lbl_VirtualBoxInstalled);
+            this.Controls.Add(this.lbl_Internet_Connection);
+            this.Controls.Add(this.lbl_mac_address);
+            this.Controls.Add(this.lbl_ssid_status);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lbl_ssid);
+            this.Controls.Add(this.lbl_getaway);
+            this.Controls.Add(this.lbl_ip);
+            this.Controls.Add(this.lbl_Hostname);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "VirtualBoxManagement";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Hostname;
+        private System.Windows.Forms.Label lbl_ip;
+        private System.Windows.Forms.Label lbl_getaway;
+        private System.Windows.Forms.Label lbl_ssid;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbl_ssid_status;
+        private System.Windows.Forms.Label lbl_mac_address;
+        private System.Windows.Forms.Label lbl_Internet_Connection;
+        private System.Windows.Forms.Label lbl_VirtualBoxInstalled;
+        private System.Windows.Forms.Label lbl_VBOX_V;
+    }
+}
+
---
+++ Form1_Tomeu_Gomila/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+
+
+
+namespace Form1_Tomeu_Gomila
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetUserName();
+            DisplayIPAddress();
+            DisplayGatewayInLabel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetUserName()
+        {
+            String UserName = Environment.UserName;
+            lbl_Username.Text = UserName;
+            string hostName = Dns.GetHostName();
+            lbl_Hostname.Text = hostName;
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+        }
+
+        private void DisplayIPAddress()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface adapter in adapters)
+            {
+                if (adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    IPInterfaceProperties ipProperties = adapter.GetIPProperties();
+
+                    foreach (UnicastIPAddressInformation address in ipProperties.UnicastAddresses)
+                    {
+                        if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            lbl_ip.Text = address.Address.ToString();
+                            return;
+                        }
+                    }
+                }
+            }
+
+            lbl_ip.Text = "IP address: Not found";
+        }
+
+        public void DisplayGatewayInLabel()
+        {
+            
+            NetworkInterface activeInterface = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(ni => ni.OperationalStatus == OperationalStatus.Up && ni.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+
+            if (activeInterface != null)
+            {
+                
+                IPInterfaceProperties ipProps = activeInterface.GetIPProperties();
+
+                
+                GatewayIPAddressInformation gateway = ipProps.GatewayAddresses.FirstOrDefault();
+
+                if (gateway != null)
+                {
+                    
+                    lbl_getaway.Text = gateway.Address.ToString();
+                }
+                else
+                {
+                    lbl_getaway.Text = "No gateway found.";
+                }
+            }
+            else
+            {
+                lbl_getaway.Text = "No active network interface found.";
+            }
+        }
+       
+    }
+}
